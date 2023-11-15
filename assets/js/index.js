84f9c43fabc32be73d7246a7ddfe0a5d4997d32c
Dev Mallone <kaull.dev@gmail.com>
2023-11-15 02:04:59 -0300

    Finalização Projeto Mallone

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,20 @@
 // BUTTON TAKEMICHI
-$(".takemichi").css("display", "none");
+
 $(".mikey").css("display", "none");
 $(".draken").css("display", "none");
+$(".chifuyu").css("display", "none");
+$(".baji").css("display", "none");
+$(".senju").css("display", "none");
+$(".hinataTachibana").css("display", "none");
 
 $(".takemichiButton").click(function () {
   $(".takemichi").css("display", "flex");
   $(".hinataTachibana").css("display", "none");
   $(".mikey").css("display", "none");
   $(".draken").css("display", "none");
+  $(".chifuyu").css("display", "none");
+  $(".baji").css("display", "none");
+  $(".senju").css("display", "none");
 });
 // BUTTON HINA
 $(".hinaButton").click(function () {
@@ -15,6 +22,9 @@ $(".hinaButton").click(function () {
   $(".hinataTachibana").css("display", "flex");
   $(".mikey").css("display", "none");
   $(".draken").css("display", "none");
+  $(".chifuyu").css("display", "none");
+  $(".baji").css("display", "none");
+  $(".senju").css("display", "none");
 });
 // BUTTON MIKEY
 $(".mikeyButton").click(function () {
@@ -22,6 +32,9 @@ $(".mikeyButton").click(function () {
   $(".hinataTachibana").css("display", "none");
   $(".mikey").css("display", "flex");
   $(".draken").css("display", "none");
+  $(".chifuyu").css("display", "none");
+  $(".baji").css("display", "none");
+  $(".senju").css("display", "none");
 });
 // BUTTON DRAKEN
 $(".drakenButton").click(function () {
@@ -29,14 +42,64 @@ $(".drakenButton").click(function () {
   $(".hinataTachibana").css("display", "none");
   $(".takemichi").css("display", "none");
   $(".mikey").css("display", "none");
+  $(".chifuyu").css("display", "none");
+  $(".baji").css("display", "none");
+  $(".senju").css("display", "none");
+});
+// BUTTON CHIFUYU
+$(".chifuyuButton").click(function () {
+  $(".draken").css("display", "none");
+  $(".hinataTachibana").css("display", "none");
+  $(".takemichi").css("display", "none");
+  $(".mikey").css("display", "none");
+  $(".baji").css("display", "none");
+  $(".chifuyu").css("display", "flex");
+  $(".senju").css("display", "none");
+});
+// BUTTON BAJI
+$(".bajiButton").click(function () {
+  $(".draken").css("display", "none");
+  $(".hinataTachibana").css("display", "none");
+  $(".takemichi").css("display", "none");
+  $(".mikey").css("display", "none");
+  $(".chifuyu").css("display", "none");
+  $(".baji").css("display", "flex");
+  $(".senju").css("display", "none");
+});
+// BUTTON SENJU
+$(".senjuButton").click(function () {
+  $(".draken").css("display", "none");
+  $(".hinataTachibana").css("display", "none");
+  $(".takemichi").css("display", "none");
+  $(".mikey").css("display", "none");
+  $(".chifuyu").css("display", "none");
+  $(".baji").css("display", "none");
+  $(".senju").css("display", "flex");
 });
 
-// let header = document.querySelector("");
+// Obtém referências para os elementos de áudio e controles personalizados
+const audio = document.getElementById("myAudio");
+const playButton = document.getElementById("playButton");
+const pauseButton = document.getElementById("pauseButton");
 
-// window.addEventListener("scroll", function () {
-//   if (window.scrollY > 10) {
-//     header.classList.add("changeLogoName");
-//   } else {
-//     header.classList.remove("changeLogoName");
-//   }
-// });
+// Adiciona evento de clique para o botão de play
+playButton.addEventListener("click", () => {
+  audio.play();
+});
+
+// Adiciona evento de clique para o botão de pause
+pauseButton.addEventListener("click", () => {
+  audio.pause();
+});
+
+$("#playButton").hide();
+
+$("#playButton").click(function () {
+  $("#playButton").hide();
+  $("#pauseButton").show();
+});
+
+$("#pauseButton").click(function () {
+  $("#playButton").show();
+  $("#pauseButton").hide();
+});
