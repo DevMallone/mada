e007ad8d00ac51fc9ae03ff2782a44959f84d5f8
DevMallone <kaullony.brito@gmail.com>
2023-12-07 09:36:33 -0300

    index

--- assets/js/index.js
+++ assets/js/index.js
@@ -103,3 +103,28 @@ $("#pauseButton").click(function () {
   $("#playButton").show();
   $("#pauseButton").hide();
 });
+
+// SLIDE
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 4,
+//   spaceBetween: 10,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     640: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     768: {
+//       slidesPerView: 4,
+//       spaceBetween: 40,
+//     },
+//     1024: {
+//       slidesPerView: 5,
+//       spaceBetween: 50,
+//     },
+//   },
+// });
